7d8aec264f0baddab43f8c9cba3ecabe44ffb1bf
giofernandezrd <124070087+giofernandezrd@users.noreply.github.com>
2024-11-24 22:33:48 -0400

    Super UPdate

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["Diseñador","Gerencia de Proyectos","Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
